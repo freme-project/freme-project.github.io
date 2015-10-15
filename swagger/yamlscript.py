525819ec5c0a65f1eb44ca464733046e067e1402
Jonathan Paul Sauder <j.sauder@campus.tu-berlin.de>
2015-10-15 12:41:52 +0200

    Updated swagger build, now has collapsible tags

--- swagger/yamlscript.py
+++ swagger/yamlscript.py
@@ -26,7 +26,7 @@ def main():
 		raise Exception("\n\tException Handled in /swagger/yamlscript.py: The YAML File at "+ os.path.dirname(__file__)+"/swagger.yaml is invalid! The generation of a simple API-Doc was skipped")	
 		sys.exit(1)
 		
-	included={
+	included_paths={
 	"/e-entity/freme-ner/documents": ["post"],
 	"/e-entity/dbpedia-spotlight/documents": ["post"],
 	"/e-publishing/html": ["post"],
@@ -37,16 +37,23 @@ def main():
 	}
 	
 	for path in full["paths"].keys():
-		if path not in included:
+		if path not in included_paths:
 			del full["paths"][path]
 		else:
-			for method in included[path]:
+			for method in included_paths[path]:
 				if method not in full["paths"][path].keys():
 					del full["paths"][path][method]
 #				else:
 #					full["paths"][path][method]['tags']=["Enrichment Endpoints"]
-	
-	full['info']['description']="This section only covers the most important endpoints of FREME: the enrichment endpoints.<br><br> The endpoints can be used to access FREME e-Services via common HTTP requests.<br><br> A full documentation of all e-Service endpoints, including all parameters, is provided <a href=\"full.html\">here</a>. For usage examples, see the <a href=\"../Tutorials/overview.html\">tutorial section</a>."
+
+	included_tags=[
+	"General Information"
+	]
+	for tag in full["tags"]:
+		if tag not in included_tags:
+			full["tags"].remove(tag)
+
+	full['paths']['General Information']="This section only covers the most important endpoints of FREME: the enrichment endpoints.<br><br> The endpoints can be used to access FREME e-Services via common HTTP requests.<br><br> A full documentation of all e-Service endpoints, including all parameters, is provided <a href=\"full.html\">here</a>. For usage examples, see the <a href=\"../Tutorials/overview.html\">tutorial section</a>."
 
 	
 	with open(os.path.dirname(__file__)+"/simple.yaml",'w') as f:
