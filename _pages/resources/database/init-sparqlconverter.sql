a07614b501ed01384cdc399260f50cea7edda8a4
Arne Binder <arne.b.binder@gmail.com>
2016-04-19 17:47:15 +0200

    added introduction sentence and modified sql scripts https://github.com/freme-project/Documentation/issues/209#issuecomment-211942183

--- _pages/resources/database/init-sparqlconverter.sql
+++ _pages/resources/database/init-sparqlconverter.sql
@@ -1,9 +1,3 @@
--- MySQL dump 10.13  Distrib 5.6.24, for Win64 (x86_64)
---
--- Host: localhost    Database: freme
--- ------------------------------------------------------
--- Server version	5.5.47-0ubuntu0.14.04.1
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -15,43 +9,18 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `sparqlconverter`
---
-
-DROP TABLE IF EXISTS `sparqlconverter`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `sparqlconverter` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `creation_time` bigint(20) NOT NULL,
-  `visibility` int(11) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `owner_name` varchar(255) DEFAULT NULL,
-  `query` longtext,
-  `description` longtext,
-  PRIMARY KEY (`id`),
-  KEY `FK_1dmqqmfrxabnpoqby9kn2lk8t` (`owner_name`),
-  CONSTRAINT `FK_1dmqqmfrxabnpoqby9kn2lk8t` FOREIGN KEY (`owner_name`) REFERENCES `user` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `sparqlconverter`
---
-
 LOCK TABLES `sparqlconverter` WRITE;
 /*!40000 ALTER TABLE `sparqlconverter` DISABLE KEYS */;
-INSERT INTO `sparqlconverter` (`id`, `creation_time`, `visibility`, `name`, `owner_name`, `query`, `description`) 
+INSERT INTO `sparqlconverter` (`creation_time`, `visibility`, `name`, `owner_name`, `query`, `description`)
 VALUES 
-(2,1450361669947,1,'extract-entities-only','admin','PREFIX itsrdf: <http://www.w3.org/2005/11/its/rdf#> SELECT ?entity WHERE {?charsequence itsrdf:taIdentRef ?entity}',NULL),
-(3,1450369215556,1,'terminology-terms-only','admin','PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\r\nselect  (lang(?anchorOf) as ?language) (str(?anchorOf) as ?anchor) ?confidence (GROUP_CONCAT(DISTINCT ?label;SEPARATOR = \", \") AS ?new_label) (GROUP_CONCAT(DISTINCT ?uri;SEPARATOR = \", \") AS ?new_uri) where {\r\n ?term <http://www.w3.org/2005/11/its/rdf#term> \"yes\".\r\n ?term <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#annotationUnit> ?annotationUnit .\r\n  ?term <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#anchorOf> ?anchorOf.\r\n ?annotationUnit rdfs:label ?label.\r\n ?annotationUnit <http://www.w3.org/2005/11/its/rdf#taConfidence> ?confidence.\r\n ?term <http://www.w3.org/2005/11/its/rdf#termInfoRef> ?uri\r\n}\r\ngroup by  ?annotationUnit ?confidence ?anchorOf ORDER by desc ( ?confidence )',NULL),
-(5,1450424556510,1,'sourcelang-targetlang','admin','PREFIX nif:   <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#> PREFIX its: <http://www.w3.org/2005/11/its/rdf#> PREFIX ontolex: <http://www.w3.org/ns/lemon/ontolex#> PREFIX skos: <http://www.w3.org/2004/02/skos/core#> PREFIX itsrdf: <http://www.w3.org/2005/11/its/rdf#> PREFIX tbx: <http://tbx2rdf.lider-project.eu/tbx#> SELECT    ?annotationString ?concept ?writtenRep (lang(?writtenRep) as ?language) WHERE {   ?annotationString a nif:RFC5147String.   ?annotationString its:termInfoRef ?concept.   ?concept tbx:definition ?definition.   ?concept a skos:Concept.   ?sense ontolex:reference ?concept.   ?entry ontolex:sense ?sense.   ?entry ontolex:canonicalForm ?canonicalForm.   ?canonicalForm ontolex:writtenRep ?writtenRep. }',NULL),
-(6,1452614687032,1,'original-and-translation','admin','SELECT ?source ?target \nWHERE {  ?s <http://www.w3.org/2005/11/its/rdf#target> ?target.  ?s <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#isString> ?source.}',NULL),
-(7,1453882303681,1,'entities-detailed-info','admin','PREFIX itsrdf: <http://www.w3.org/2005/11/its/rdf#> prefix nif:  <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#> SELECT ?uri ?ident ?class ?string ?charbegin ?charend WHERE {?uri itsrdf:taIdentRef ?ident. ?uri itsrdf:taClassRef ?class. ?uri nif:anchorOf ?string. ?uri nif:beginIndex ?charbegin. ?uri nif:endIndex ?charend.}',NULL),
-(9,1455818983489,1,'place-and-lat-long','admin','PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> SELECT ?place ?lat ?long WHERE {  ?place <http://www.w3.org/2003/01/geo/wgs84_pos#lat> ?lat.  ?place <http://www.w3.org/2003/01/geo/wgs84_pos#long> ?long.  sparqlconverter (datatype(?lat) = xsd:double)  sparqlconverter (datatype(?long) = xsd:double)}',NULL),
-(10,1455823838152,1,'museums-nearby','admin','SELECT ?place ?museum WHERE {  ?museum <http://xmlns.com/foaf/0.1/based_near> ?place.} ORDER by ?place',NULL),
-(11,1456923082146,1,'freme-workflow-editor-terminology','admin','\nPREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\nselect  (lang(?anchorOf) as ?language) (str(?anchorOf) as ?anchor) (SAMPLE(?bIndex) as ?beginIndex) (SAMPLE(?eIndex) as ?endIndex) ?confidence (GROUP_CONCAT(DISTINCT ?label;SEPARATOR = \", \") AS ?new_label) (GROUP_CONCAT(DISTINCT ?uri;SEPARATOR = \", \") AS ?new_uri) where {\n ?term <http://www.w3.org/2005/11/its/rdf#term> \"yes\".\n ?term <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#annotationUnit> ?annotationUnit .\n  ?term <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#anchorOf> ?anchorOf.   \n  ?term <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#beginIndex> ?bIndex.    \n  ?term <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#endIndex> ?eIndex.\n ?annotationUnit rdfs:label ?label.\n ?annotationUnit <http://www.w3.org/2005/11/its/rdf#taConfidence> ?confidence.\n ?term <http://www.w3.org/2005/11/its/rdf#termInfoRef> ?uri\n}\ngroup by  ?annotationUnit ?confidence ?anchorOf ORDER by desc ( ?confidence )',NULL);
+(1450361669947,1,'extract-entities-only','admin','PREFIX itsrdf: <http://www.w3.org/2005/11/its/rdf#> SELECT ?entity WHERE {?charsequence itsrdf:taIdentRef ?entity}',NULL),
+(1450369215556,1,'terminology-terms-only','admin','PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\r\nselect  (lang(?anchorOf) as ?language) (str(?anchorOf) as ?anchor) ?confidence (GROUP_CONCAT(DISTINCT ?label;SEPARATOR = \", \") AS ?new_label) (GROUP_CONCAT(DISTINCT ?uri;SEPARATOR = \", \") AS ?new_uri) where {\r\n ?term <http://www.w3.org/2005/11/its/rdf#term> \"yes\".\r\n ?term <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#annotationUnit> ?annotationUnit .\r\n  ?term <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#anchorOf> ?anchorOf.\r\n ?annotationUnit rdfs:label ?label.\r\n ?annotationUnit <http://www.w3.org/2005/11/its/rdf#taConfidence> ?confidence.\r\n ?term <http://www.w3.org/2005/11/its/rdf#termInfoRef> ?uri\r\n}\r\ngroup by  ?annotationUnit ?confidence ?anchorOf ORDER by desc ( ?confidence )',NULL),
+(1450424556510,1,'sourcelang-targetlang','admin','PREFIX nif:   <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#> PREFIX its: <http://www.w3.org/2005/11/its/rdf#> PREFIX ontolex: <http://www.w3.org/ns/lemon/ontolex#> PREFIX skos: <http://www.w3.org/2004/02/skos/core#> PREFIX itsrdf: <http://www.w3.org/2005/11/its/rdf#> PREFIX tbx: <http://tbx2rdf.lider-project.eu/tbx#> SELECT    ?annotationString ?concept ?writtenRep (lang(?writtenRep) as ?language) WHERE {   ?annotationString a nif:RFC5147String.   ?annotationString its:termInfoRef ?concept.   ?concept tbx:definition ?definition.   ?concept a skos:Concept.   ?sense ontolex:reference ?concept.   ?entry ontolex:sense ?sense.   ?entry ontolex:canonicalForm ?canonicalForm.   ?canonicalForm ontolex:writtenRep ?writtenRep. }',NULL),
+(1452614687032,1,'original-and-translation','admin','SELECT ?source ?target \nWHERE {  ?s <http://www.w3.org/2005/11/its/rdf#target> ?target.  ?s <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#isString> ?source.}',NULL),
+(1453882303681,1,'entities-detailed-info','admin','PREFIX itsrdf: <http://www.w3.org/2005/11/its/rdf#> prefix nif:  <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#> SELECT ?uri ?ident ?class ?string ?charbegin ?charend WHERE {?uri itsrdf:taIdentRef ?ident. ?uri itsrdf:taClassRef ?class. ?uri nif:anchorOf ?string. ?uri nif:beginIndex ?charbegin. ?uri nif:endIndex ?charend.}',NULL),
+(1455818983489,1,'place-and-lat-long','admin','PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> SELECT ?place ?lat ?long WHERE {  ?place <http://www.w3.org/2003/01/geo/wgs84_pos#lat> ?lat.  ?place <http://www.w3.org/2003/01/geo/wgs84_pos#long> ?long.  sparqlconverter (datatype(?lat) = xsd:double)  sparqlconverter (datatype(?long) = xsd:double)}',NULL),
+(1455823838152,1,'museums-nearby','admin','SELECT ?place ?museum WHERE {  ?museum <http://xmlns.com/foaf/0.1/based_near> ?place.} ORDER by ?place',NULL),
+(1456923082146,1,'freme-workflow-editor-terminology','admin','\nPREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\nselect  (lang(?anchorOf) as ?language) (str(?anchorOf) as ?anchor) (SAMPLE(?bIndex) as ?beginIndex) (SAMPLE(?eIndex) as ?endIndex) ?confidence (GROUP_CONCAT(DISTINCT ?label;SEPARATOR = \", \") AS ?new_label) (GROUP_CONCAT(DISTINCT ?uri;SEPARATOR = \", \") AS ?new_uri) where {\n ?term <http://www.w3.org/2005/11/its/rdf#term> \"yes\".\n ?term <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#annotationUnit> ?annotationUnit .\n  ?term <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#anchorOf> ?anchorOf.   \n  ?term <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#beginIndex> ?bIndex.    \n  ?term <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#endIndex> ?eIndex.\n ?annotationUnit rdfs:label ?label.\n ?annotationUnit <http://www.w3.org/2005/11/its/rdf#taConfidence> ?confidence.\n ?term <http://www.w3.org/2005/11/its/rdf#termInfoRef> ?uri\n}\ngroup by  ?annotationUnit ?confidence ?anchorOf ORDER by desc ( ?confidence )',NULL);
 /*!40000 ALTER TABLE `sparqlconverter` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -63,5 +32,3 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-04-19 14:28:44
--- _pages/resources/database/init-template.sql
+++ _pages/resources/database/init-template.sql
@@ -1,8 +1,3 @@
--- MySQL dump 10.13  Distrib 5.6.24, for Win64 (x86_64)
---
--- Host: localhost    Database: freme
--- ------------------------------------------------------
--- Server version	5.5.47-0ubuntu0.14.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -15,45 +10,18 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `template`
---
-
-DROP TABLE IF EXISTS `template`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `template` (
-  `id` bigint(11) NOT NULL AUTO_INCREMENT,
-  `visibility` int(11) DEFAULT NULL,
-  `owner_name` varchar(255) DEFAULT NULL,
-  `description` longtext,
-  `endpoint` longtext,
-  `label` longtext,
-  `query` longtext,
-  `endpoint_type` int(11) DEFAULT NULL,
-  `creation_time` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_sn2iyndm5xit6hp99ra2cjcc` (`owner_name`),
-  CONSTRAINT `FK_sn2iyndm5xit6hp99ra2cjcc` FOREIGN KEY (`owner_name`) REFERENCES `user` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=4495 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `template`
---
-
 LOCK TABLES `template` WRITE;
 /*!40000 ALTER TABLE `template` DISABLE KEYS */;
-INSERT INTO `template` (`id`, `visibility`, `owner_name`, `description`, `endpoint`, `label`, `query`, `endpoint_type`, `creation_time`) 
+INSERT INTO `template` (`visibility`, `owner_name`, `description`, `endpoint`, `label`, `query`, `endpoint_type`, `creation_time`)
 VALUES 
-(1,1,'admin','This template enriches with a list of museums (max 10) within a 50km radius around each location entity.','http://live.dbpedia.org/sparql/','Find nearest museums','PREFIX dbpedia: <http://dbpedia.org/resource/> PREFIX dbo: <http://dbpedia.org/ontology/> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> CONSTRUCT {  ?museum <http://xmlns.com/foaf/0.1/based_near> <@@@entity_uri@@@> . } WHERE {  <@@@entity_uri@@@> geo:geometry ?citygeo .  OPTIONAL { ?museum rdf:type dbo:Museum . }  ?museum geo:geometry ?museumgeo .  FILTER (<bif:st_intersects>(?museumgeo, ?citygeo, 50)) } LIMIT 10',0,0),
-(2,1,'admin','The template retrieves events (max 10) related to a place. The information is retrieved from the DBpedia live SPARQL endpoint.','http://live.dbpedia.org/sparql','Events related to a place','PREFIX dbpedia: <http://dbpedia.org/resource/> PREFIX dbpedia-owl: <http://dbpedia.org/ontology/> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> CONSTRUCT { ?event <http://dbpedia.org/ontology/place> <@@@entity_uri@@@> . } WHERE { ?event rdf:type <http://dbpedia.org/ontology/Event> .  ?event <http://dbpedia.org/ontology/place> <@@@entity_uri@@@> .  } LIMIT 10',0,0),
-(3,1,'admin','','http://factforge.net/sparql','dataset label','PREFIX geo-pos: <http://www.w3.org/2003/01/geo/wgs84_pos#> CONSTRUCT { <@@@entity_uri@@@> geo-pos:lat ?lat . <@@@entity_uri@@@> geo-pos:long ?long . } WHERE { <@@@entity_uri@@@> geo-pos:lat ?lat . <@@@entity_uri@@@> geo-pos:long ?long . }',0,0),
-(5,1,'admin','This template enriches with a list of persons (max 10) born in locations provided in the text. This template is using Europeana SPARQL endpoint as source for information.','http://europeana.ontotext.com/sparql','Authors born in place','PREFIX edm: <http://www.europeana.eu/schemas/edm/> PREFIX ore: <http://www.openarchives.org/ore/terms/> CONSTRUCT {  ?person  <http://rdvocab.info/ElementsGr2/placeOfBirth> <@@@entity_uri@@@> ;  } WHERE {  ?person  <http://rdvocab.info/ElementsGr2/placeOfBirth> <@@@entity_uri@@@> ;  } LIMIT 10',0,0),
-(6,1,'admin','','http://europeana.ontotext.com/sparql','','PREFIX dbpedia: <http://dbpedia.org/resource/> PREFIX dbpedia-owl: <http://dbpedia.org/ontology/> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> CONSTRUCT { ?event <http://dbpedia.org/ontology/place> <@@@entity_uri@@@> . } WHERE { ?event rdf:type <http://dbpedia.org/ontology/@@@event_type@@@> .  ?event <http://dbpedia.org/ontology/place> <@@@entity_uri@@@> .  } LIMIT 10',0,0),
-(7,1,'admin','This template retrieves bakeries near a place within radius of 10 km. The places should be identified with DBpedia or GeoNames URIs.','http://linkedgeodata.org/sparql/','Bakeries near a place','Prefix lgdo: <http://linkedgeodata.org/ontology/> Prefix geom: <http://geovocab.org/geometry#> Prefix ogc: <http://www.opengis.net/ont/geosparql#> Prefix owl: <http://www.w3.org/2002/07/owl#> CONSTRUCT {  ?b <http://xmlns.com/foaf/0.1/based_near> <@@@entity_uri@@@> .  ?b <http://www.w3.org/2000/01/rdf-schema#label> ?label . } WHERE {  ?c    owl:sameAs <@@@entity_uri@@@> ;    geom:geometry [      ogc:asWKT ?cg    ] .  ?b    a lgdo:Bakery ;    <http://www.w3.org/2000/01/rdf-schema#label> ?label ;        geom:geometry [      ogc:asWKT ?bg    ] .    Filter(<bif:st_intersects>(?cg, ?bg, 10)) .} Limit 10',0,0),
-(4447,1,'admin','This template is using a Linked Data Frament endpoint to fetch DBpedia categories for a DBpedia resource.','http://rv2622.1blu.de:5000/dbpedia-categories','DBpedia Categories via LDF','CONSTRUCT {  <@@@entity_uri@@@> <http://purl.org/dc/terms/subject> ?category .  ?category <http://www.w3.org/2000/01/rdf-schema#label> ?label } WHERE { <@@@entity_uri@@@> <http://purl.org/dc/terms/subject> ?category . ?category <http://www.w3.org/2000/01/rdf-schema#label> ?label }',1,1446716795534),
-(4448,1,'admin','Retrieve all bakeries 10km close to a place.','http://linkedgeodata.org/vsnorql','Linked Geo Data','  PREFIX owl: <http://www.w3.org/2002/07/owl# >	PREFIX ogc: <http://www.opengis.net/ont/geosparql# >	PREFIX geom: <http://geovocab.org/geometry# >	PREFIX lgdo: <http://linkedgeodata.org/ontology/ >	CONSTRUCT {	?s <http://xmlns.com/foaf/0.1/based_near > <@@@entity_uri@@@ > .	} WHERE {	?s	a lgdo:Bakery ;	geom:geometry [ ogc:asWKT ?sg ] .	?a	owl:sameAs <@@@entity_uri@@@ > ;	geom:geometry [ ogc:asWKT ?ag ] .	FILTER(bif:st_intersects(?sg, ?ag, 10))	} LIMIT 10 ',0,1447063868052),
+(1,'admin','This template enriches with a list of museums (max 10) within a 50km radius around each location entity.','http://live.dbpedia.org/sparql/','Find nearest museums','PREFIX dbpedia: <http://dbpedia.org/resource/> PREFIX dbo: <http://dbpedia.org/ontology/> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> CONSTRUCT {  ?museum <http://xmlns.com/foaf/0.1/based_near> <@@@entity_uri@@@> . } WHERE {  <@@@entity_uri@@@> geo:geometry ?citygeo .  OPTIONAL { ?museum rdf:type dbo:Museum . }  ?museum geo:geometry ?museumgeo .  FILTER (<bif:st_intersects>(?museumgeo, ?citygeo, 50)) } LIMIT 10',0,0),
+(1,'admin','The template retrieves events (max 10) related to a place. The information is retrieved from the DBpedia live SPARQL endpoint.','http://live.dbpedia.org/sparql','Events related to a place','PREFIX dbpedia: <http://dbpedia.org/resource/> PREFIX dbpedia-owl: <http://dbpedia.org/ontology/> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> CONSTRUCT { ?event <http://dbpedia.org/ontology/place> <@@@entity_uri@@@> . } WHERE { ?event rdf:type <http://dbpedia.org/ontology/Event> .  ?event <http://dbpedia.org/ontology/place> <@@@entity_uri@@@> .  } LIMIT 10',0,0),
+(1,'admin','','http://factforge.net/sparql','dataset label','PREFIX geo-pos: <http://www.w3.org/2003/01/geo/wgs84_pos#> CONSTRUCT { <@@@entity_uri@@@> geo-pos:lat ?lat . <@@@entity_uri@@@> geo-pos:long ?long . } WHERE { <@@@entity_uri@@@> geo-pos:lat ?lat . <@@@entity_uri@@@> geo-pos:long ?long . }',0,0),
+(1,'admin','This template enriches with a list of persons (max 10) born in locations provided in the text. This template is using Europeana SPARQL endpoint as source for information.','http://europeana.ontotext.com/sparql','Authors born in place','PREFIX edm: <http://www.europeana.eu/schemas/edm/> PREFIX ore: <http://www.openarchives.org/ore/terms/> CONSTRUCT {  ?person  <http://rdvocab.info/ElementsGr2/placeOfBirth> <@@@entity_uri@@@> ;  } WHERE {  ?person  <http://rdvocab.info/ElementsGr2/placeOfBirth> <@@@entity_uri@@@> ;  } LIMIT 10',0,0),
+(1,'admin','','http://europeana.ontotext.com/sparql','','PREFIX dbpedia: <http://dbpedia.org/resource/> PREFIX dbpedia-owl: <http://dbpedia.org/ontology/> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> CONSTRUCT { ?event <http://dbpedia.org/ontology/place> <@@@entity_uri@@@> . } WHERE { ?event rdf:type <http://dbpedia.org/ontology/@@@event_type@@@> .  ?event <http://dbpedia.org/ontology/place> <@@@entity_uri@@@> .  } LIMIT 10',0,0),
+(1,'admin','This template retrieves bakeries near a place within radius of 10 km. The places should be identified with DBpedia or GeoNames URIs.','http://linkedgeodata.org/sparql/','Bakeries near a place','Prefix lgdo: <http://linkedgeodata.org/ontology/> Prefix geom: <http://geovocab.org/geometry#> Prefix ogc: <http://www.opengis.net/ont/geosparql#> Prefix owl: <http://www.w3.org/2002/07/owl#> CONSTRUCT {  ?b <http://xmlns.com/foaf/0.1/based_near> <@@@entity_uri@@@> .  ?b <http://www.w3.org/2000/01/rdf-schema#label> ?label . } WHERE {  ?c    owl:sameAs <@@@entity_uri@@@> ;    geom:geometry [      ogc:asWKT ?cg    ] .  ?b    a lgdo:Bakery ;    <http://www.w3.org/2000/01/rdf-schema#label> ?label ;        geom:geometry [      ogc:asWKT ?bg    ] .    Filter(<bif:st_intersects>(?cg, ?bg, 10)) .} Limit 10',0,0),
+(1,'admin','This template is using a Linked Data Frament endpoint to fetch DBpedia categories for a DBpedia resource.','http://rv2622.1blu.de:5000/dbpedia-categories','DBpedia Categories via LDF','CONSTRUCT {  <@@@entity_uri@@@> <http://purl.org/dc/terms/subject> ?category .  ?category <http://www.w3.org/2000/01/rdf-schema#label> ?label } WHERE { <@@@entity_uri@@@> <http://purl.org/dc/terms/subject> ?category . ?category <http://www.w3.org/2000/01/rdf-schema#label> ?label }',1,1446716795534),
+(1,'admin','Retrieve all bakeries 10km close to a place.','http://linkedgeodata.org/vsnorql','Linked Geo Data','  PREFIX owl: <http://www.w3.org/2002/07/owl# >	PREFIX ogc: <http://www.opengis.net/ont/geosparql# >	PREFIX geom: <http://geovocab.org/geometry# >	PREFIX lgdo: <http://linkedgeodata.org/ontology/ >	CONSTRUCT {	?s <http://xmlns.com/foaf/0.1/based_near > <@@@entity_uri@@@ > .	} WHERE {	?s	a lgdo:Bakery ;	geom:geometry [ ogc:asWKT ?sg ] .	?a	owl:sameAs <@@@entity_uri@@@ > ;	geom:geometry [ ogc:asWKT ?ag ] .	FILTER(bif:st_intersects(?sg, ?ag, 10))	} LIMIT 10 ',0,1447063868052),
 /*!40000 ALTER TABLE `template` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -66,4 +34,3 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-04-19 14:28:42
