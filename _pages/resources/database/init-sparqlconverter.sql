3bc5a8dac9f893ff53a081c52a401c74e4e87d00
ArneBinder <ArneBinder@users.noreply.github.com>
2016-04-26 17:29:48 +0200

    #209 removed LOCK / UNLOCK tables

--- _pages/resources/database/init-sparqlconverter.sql
+++ _pages/resources/database/init-sparqlconverter.sql
@@ -9,7 +9,6 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-LOCK TABLES `sparqlconverter` WRITE;
 /*!40000 ALTER TABLE `sparqlconverter` DISABLE KEYS */;
 INSERT INTO `sparqlconverter` (`creation_time`, `visibility`, `name`, `owner_name`, `query`, `description`)
 VALUES 
@@ -22,7 +21,6 @@ VALUES
 (0,1,'museums-nearby','admin','SELECT ?place ?museum WHERE {  ?museum <http://xmlns.com/foaf/0.1/based_near> ?place.} ORDER by ?place',NULL),
 (0,1,'freme-workflow-editor-terminology','admin','\nPREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\nselect  (lang(?anchorOf) as ?language) (str(?anchorOf) as ?anchor) (SAMPLE(?bIndex) as ?beginIndex) (SAMPLE(?eIndex) as ?endIndex) ?confidence (GROUP_CONCAT(DISTINCT ?label;SEPARATOR = \", \") AS ?new_label) (GROUP_CONCAT(DISTINCT ?uri;SEPARATOR = \", \") AS ?new_uri) where {\n ?term <http://www.w3.org/2005/11/its/rdf#term> \"yes\".\n ?term <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#annotationUnit> ?annotationUnit .\n  ?term <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#anchorOf> ?anchorOf.   \n  ?term <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#beginIndex> ?bIndex.    \n  ?term <http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#endIndex> ?eIndex.\n ?annotationUnit rdfs:label ?label.\n ?annotationUnit <http://www.w3.org/2005/11/its/rdf#taConfidence> ?confidence.\n ?term <http://www.w3.org/2005/11/its/rdf#termInfoRef> ?uri\n}\ngroup by  ?annotationUnit ?confidence ?anchorOf ORDER by desc ( ?confidence )',NULL);
 /*!40000 ALTER TABLE `sparqlconverter` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
