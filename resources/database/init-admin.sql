ddf02510c63cb04dca15e3315dc98ac042ad0ab7
ArneBinder <ArneBinder@users.noreply.github.com>
2016-08-02 12:00:06 +0200

    Update init-admin.sql

--- resources/database/init-admin.sql
+++ resources/database/init-admin.sql
@@ -1,1 +1,1 @@
-INSERT INTO `user` (`name`, `password`, `role`) VALUES ('admin','CHANGE_THIS_PASSWORD','ROLE_ADMIN');
+INSERT INTO `user` (`name`, `password`, `role`) VALUES ('admin','WFgsIiunMKc74OleOtehpzO621iG91WY2lY35X7QYNc=$TaMA8BGbhP2zuE7PRvgjGNqpMdNUSa8KwaW/wcf8qIM=','ROLE_ADMIN');
